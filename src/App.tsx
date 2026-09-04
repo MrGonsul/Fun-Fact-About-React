e4fe409c52510e041a3a9491e500ed14a391c9cf
Mr.Gonsul <abdalmohimn68@gmail.com>
2026-09-04 14:27:30 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Main from "./Components/main"
+import Navbar from "./Components/Nav"
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
---
+++ src/Components/nav.tsx
@@ -0,0 +1,13 @@
+//import logo from "../logo.png"
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                {/* <img src={logo} alt="React logo" /> */}
+                <img src="/logo.png"  />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
